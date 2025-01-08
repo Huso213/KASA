f9f1079103d937dff97a1831d15a40acc7344d00
Huso213 <101049695+Huso213@users.noreply.github.com>
2025-01-08 03:41:52 +0100

    media querryfini

--- src/menpage/Main.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import"./Main.scss";
-
-function Main({children}){//Fonction Main prend les enfants en paramètres
-    return <div className="Main">{children}</div>;
-}
-
-export default Main;
