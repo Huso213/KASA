71e816648502387d1018fa1b4eae12b8436d023c
Huso213 <101049695+Huso213@users.noreply.github.com>
2025-01-10 19:02:43 +0100

    mise en place page appart

--- src/pages/Appartementpage.jsx
+++ src/pages/Appartementpage.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react";
+import "../pages/Appartement.scss";
+
 import Banner from "../components/Banner";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-
 function Appartement() {
   return (
     <>
-    <Navbar />
-      <Banner />
-      <Footer />
+      <div className="pageappart">
+        <Navbar />
+        <Footer />
+      </div>
     </>
   );
-} 
+}
 export default Appartement;
