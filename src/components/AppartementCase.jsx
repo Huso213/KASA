f41421ed4c856d86158750dd9e080b5f44894687
Huso213 <101049695+Huso213@users.noreply.github.com>
2025-01-11 00:47:07 +0100

    debut de la stylisation page appart

--- src/components/AppartementCase.jsx
+++ src/components/AppartementCase.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import "./AppartementCase.scss";
 import Appartement from "./Appartement.jsx";
+//import utilAppartements from "../logement/utilAppartements.jsx";
+
+//Je recupere les données du json
+fetch("/info.json")
+.then((res) => res.json())
+.then((res) => console.log(res))
+.catch(console.error);//pour les erreur de connexion
 function AppartementCase() {
-  return (
+ 
+return (
     /*Couleur de fond */
     <div className="AppartementCase">
       {/* Carte des appartements */}
--- src/pages/Appartementpage.jsx
+++ src/pages/Appartementpage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../pages/Appartement.scss";
+import "../pages/Appartementpage.scss";
 
 import Banner from "../components/Banner";
 import Navbar from "../components/Navbar";
@@ -9,6 +9,16 @@ function Appartement() {
     <>
       <div className="pageappart">
         <Navbar />
+        <div className="imgappart"><img src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg" alt="" /></div>
+        <div className="textapparttitre">
+          <h1>Cozy loft on the Canal Saint Martin</h1>
+          <h2>Paris, 75010</h2></div>
+          <div className="textapparttitresous">
+
+          <p>Cozy</p>
+          <p>Canal</p>
+          <p>Paris 10</p>
+        </div>
         <Footer />
       </div>
     </>
