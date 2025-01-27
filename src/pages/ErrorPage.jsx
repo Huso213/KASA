debc8c584c0bd353a449c28cc3d9b3d77d97f7d5
Huso213 <101049695+Huso213@users.noreply.github.com>
2025-01-27 21:57:01 +0100

    responsive fini

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -7,8 +7,8 @@ import { Link } from 'react-router-dom';
 export function ErrorPage() {
   return (
     <>
-      <Navbar />
       <div className="error-page">
+      <Navbar />
         <h1>404</h1>
         <p>Oups! La page que vous demandez n'existe pas.</p>
         <Link to="/">Retourner sur la page d’accueil</Link>
